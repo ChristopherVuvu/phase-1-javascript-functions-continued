4c96af367bd7a10aadd8fbf78c6e61a50063aa00
ChristopherVuvu <vuvumumo93@gmail.com>
2023-03-25 07:35:14 +0300

    done assignement

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(game = "roller-skate"){
+    console.log(`This Saturday, i want to ${game}!`);
+}
+
+function mondayWork(role = "go to the office"){
+    console.log(`I will ${role}.`)
+}
+
+function wrapAdjective(name = "special"){
+    const greeting1 = "You are ";
+    return function(){
+        const greeting2 = "a dedicated programmer.";
+        return function(){
+            console.log(`${greeting1}${name}${greeting2}`);
+        };
+    };
+}
+console.log(wrapAdjective("Mumo")()());
